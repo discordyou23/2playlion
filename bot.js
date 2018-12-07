9788a9e1ea9642b37ddb3c73cc7e1084bd6f0a08
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-12-07 13:44:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const convert = require("hh-mm-ss")
-const dateFormat = require('dateformat');
-const fs = require('fs');
-const pretty = require('pretty-ms');
-const rn = require('random-number');
-const moment = require('moment');
 var Canvas = require('canvas')
 var jimp = require('jimp')
 
