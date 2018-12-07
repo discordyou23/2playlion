fdc18d6be16fa4660a1dbc904b9962174125d5e7
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-12-07 14:20:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,6 @@ client.on('guildMemberAdd', member => {
         .setThumbnail(m.avatarURL)
         .setAuthor(m.username,m.avatarURL)
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-        .addFiled('🕊**__Welcome To Silver Community__**🕊')
          .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:yumz});          
          
